db5c5078429f1639d314498c5ce2cb5143c1cdfe
swarna <baruaswarna0@gmail.com>
2023-01-04 04:33:35 -0800

    cal

---
+++ main.js
@@ -0,0 +1,198 @@
+/*
+//var
+
+//var name= "Swarna Barua",
+//    roll=367874 ;
+//    name="tuhin";
+
+//let
+
+//let name="swarna";
+//     name= "Tuhin";
+//let roll=588791;
+//    roll= 45666;  
+
+//const
+
+const name="swarna barua";
+//       name="barna";
+const roll= 374547;
+//      roll=4955;       
+console.log(name , roll)    
+*/
+//operator
+
+/*
+let number1=20;
+let number2=20;
+console.log(number1+number2)  
+console.log(number1-number2)   
+console.log(number1*number2) 
+console.log(number1/number2) 
+console.log(number1%number2) 
+
+let firstName="Swarna";
+let lastName="Barua";
+document.write(firstName +" "+lastName)
+
+document.write("Name:Swarna Barua<br>");
+document.write("Roll:588791<br>");
+document.write("Department:Computer<br>");
+document.write("Group:B1<br>");
+
+*/
+
+document.getElementById("small_scren").innerHTML = "shunu";
+
+//object
+/*
+const personDetails={
+       students1 : {
+       Name:"Swarna Barua",
+       Roll:588791,
+       Department:"Computer"
+       },
+       students2 : {
+              Name:{
+                   firstName:"Swarna",
+                   lastName:"Barua",  
+              },
+              Roll:588791,
+              Department:"Computer"
+              }
+}
+let studentFullName=personDetails.students2.Name.firstName;
+let studentFullName2=personDetails.students2.Name.lastName;
+console.log(studentFullName +" "+ studentFullName2)
+//console.log(personDetails.students2.lastName)
+
+//console.warn(personDetails)
+*/
+
+//prompt---
+
+//let userName= prompt("Enter your name:"," ")
+//console.log(userName)
+
+//let userName= prompt("Enter your name:","Swarna Barua")
+//console.log(userName)
+
+//Confirm---
+
+//let confirmValue=confirm("Do you stay here?");
+//console.log(confirmValue)
+
+//Arithmetics---
+
+//let sumNum=10;
+//console.log(sumNum++)
+
+//let decNum=10;
+//console.log(++decNum)
+
+//let subNum=10;
+//console.log(subNum--)
+
+//let subsNum=10;
+//console.log(--subsNum)
+
+//Assignment operator---
+
+//let x = 10;
+//let y = 30;
+//console.log(x===y)
+//console.log(x===x+y)
+//console.log(x===x-y)
+//console.log(x===x*y)
+//console.log(x===x%y)
+//console.log(x===x/y)
+
+//if else if/else
+
+//let numOne = 30;
+//let numTwo = 20;
+//let operator=prompt("enter your operator"," ");
+
+//if(numOne<numTwo){
+//     console.log("true")
+
+//}else{
+//     console.log("false")
+//}
+
+/*
+if(operator==="+"){
+     let sum=numOne+numTwo;
+     console.log(sum)
+}else if(operator==="-"){
+     let sub= numTwo-numOne;
+     console.log(sub)
+}else if(operator==="*"){
+     let multi= numOne*numTwo;
+     console.log(multi)
+}else if (operator==="/") {
+     let div=numTwo/numOne;
+     console.log(div)
+}else{
+     console.log("error")
+}
+*/
+
+/*
+if(operator!="+"){
+     let sum=numOne-numTwo;
+     console.log(sum)
+}
+//else if(operator==="-"){
+ //    let sub= numTwo-numOne;
+ //    console.log(sub)
+//}
+else if(operator==="*"){
+     let multi= numOne*numTwo;
+     console.log(multi)
+}else if (operator==="/") {
+     let div=numTwo/numOne;
+     console.log(div)
+}else{
+     console.log("error")
+}
+*/
+
+/*
+if(operator==="+"){
+     
+     let numOneCheck = numOne>0 ? numOne:"false";
+     let numTwoCheck = numTwo>0 ? numTwo:"false";
+
+     if(numOneCheck==="false"){
+          console.log("error")
+
+
+     }
+     else if(numTwoCheck==="false"){
+          console.log("error")
+     }
+     else{
+          let sum =numOne+numTwo;
+          console.log(sum)
+     }
+}
+*/
+
+
+//else if(operator==="-"){
+ //    let sub= numTwo-numOne;
+ //    console.log(sub)
+//}
+/*
+else if(operator==="*"){
+     let multi= numOne*numTwo;
+     console.log(multi)
+}else if (operator==="/") {
+     let div=numTwo/numOne;
+     console.log(div)}
+*/
+//else{
+ //    console.log("error")
+//}
+
